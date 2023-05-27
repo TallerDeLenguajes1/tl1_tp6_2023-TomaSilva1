ba077402521ca848b5e8a43fc2e55d9ab274e0c9
TomaSilva1 <tomasilva302@gmail.com>
2023-05-26 23:04:51 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+int c;
+bool dato = false;
+
+while (!dato)
+{
+    Console.WriteLine("Ingrese un número: ");
+    string tipo = Console.ReadLine();
+
+    dato = int.TryParse(tipo, out c);
+
+    if (dato)
+    {
+        Console.WriteLine("El número ingresado es: " + c);
+        if(c > 0){
+            Console.WriteLine("Inversion!!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("El valor ingresado no es un número entero válido.");
+    }
+}
